9e03f6648d1a8686224756fc2cb5002fdc02b155
Micheller24 <105686334+Micheller24@users.noreply.github.com>
2023-10-30 16:49:32 -0600

    proyect

---
+++ Proyecto_DesarrolloWeb-master/Proyecto/src/main/java/com/aparta/service/impl/newpackage/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.aparta.service.impl.newpackage;
+
+/**
+ *
+ * @author Usuario
+ */
+public class NewClass {
+    
+}
